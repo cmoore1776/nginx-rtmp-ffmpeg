e79e9cbabb83ed7d501e397eacc80a5bb81eee59
Christian Moore <christian@christianmoore.me>
2018-06-23 13:49:10 -0400

    cbf/cfr

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ rtmp {
     application livein {
       live on;
       record off;
-      exec ffmpeg -i "rtmp://127.0.0.1/livein/${STREAM_KEY}" -b:v ${BITRATE}K -minrate ${BITRATE}K -maxrate ${BITRATE}K -bufsize ${BITRATE}K -s ${RESOLUTION} -c:v libx264 -preset ${PRESET} -profile:v ${PROFILE} -level:v ${LEVEL} -r ${FRAMERATE} -g ${FRAMERATE_2X} -keyint_min ${FRAMERATE} -x264opts "keyint=${FRAMERATE_2X}:min-keyint=${FRAMERATE_2X}:no-scenecut" -sws_flags lanczos -tune film -pix_fmt yuv420p -c:a copy -f flv -threads ${THREADS} -strict normal "rtmp://127.0.0.1/liveout/${STREAM_KEY}";
+      exec ffmpeg -i "rtmp://127.0.0.1/livein/${STREAM_KEY}" -b:v ${BITRATE}K -minrate ${BITRATE}K -maxrate ${BITRATE}K -bufsize ${BITRATE}K -s ${RESOLUTION} -c:v libx264 -preset ${PRESET} -profile:v ${PROFILE} -level:v ${LEVEL} -r ${FRAMERATE} -g ${FRAMERATE_2X} -keyint_min ${FRAMERATE} -x264opts "nal-hrd=cbr:force-cfr=1:keyint=${FRAMERATE_2X}:min-keyint=${FRAMERATE_2X}:no-scenecut" -sws_flags lanczos -tune film -pix_fmt yuv420p -c:a copy -f flv -threads ${THREADS} -strict normal "rtmp://127.0.0.1/liveout/${STREAM_KEY}";
     }
 
     application liveout {
